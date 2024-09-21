352e7019a5bc2d65321ff48762665b6e42155572
youssef emad eldin <youssefemadeldn>
2024-09-21 17:15:31 +0300

    delete logic

--- lib/features/auth/data/data_source/remote/auth_remote_ds.dart
+++
@@ -1,6 +0,0 @@
-import 'package:e_commerce/features/auth/domain/entity/sign_up_entity.dart';
-
-abstract class AuthRemoteDs {
-  Future<bool> signIn(String email, String password);
-  Future<bool> signUp(SignUpEntity signUpEntity);
-}
--- lib/features/auth/domain/repository/auth_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:e_commerce/features/auth/domain/entity/sign_up_entity.dart';
-
-abstract class AuthRepo {
-  Future<bool> signIn(String email, String password);
-  Future<bool> signUp(SignUpEntity signUpEntity);
-}
